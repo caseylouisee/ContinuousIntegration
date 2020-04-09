ec0ecb10d74a2742e17c72f08f1a2637a84c48e2
Casey Denner <caseylouisee@me.com>
2020-04-09 16:23:33 +0100

    first commit

---
+++ src/main/java/softwaretesting/continuousintegration/ContinuousintegrationApplication.java
@@ -0,0 +1,13 @@
+package softwaretesting.continuousintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuousintegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softwaretesting/continuousintegration/ContinuousintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package softwaretesting.continuousintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousintegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
