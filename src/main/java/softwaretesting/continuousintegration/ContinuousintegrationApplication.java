3056966126502ff9266436b69d05d3be83f18822
caseydenner <caseylouisee@me.com>
2020-06-30 18:08:20 +0100

    To fix war

--- src/main/java/softwaretesting/continuousintegration/ContinuousintegrationApplication.java
+++ src/main/java/softwaretesting/continuousintegration/ContinuousintegrationApplication.java
@@ -2,12 +2,19 @@ package softwaretesting.continuousintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContinuousintegrationApplication {
+public class ContinuousintegrationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ContinuousintegrationApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ContinuousintegrationApplication.class);
     }
 
 }
